77ea9733de2be8731d24250de1ef4fe48d449950
Keshav Nalam <nknnkumar@yahoo.com>
2026-08-24 12:56:02 +0530

    vertical 5

---
+++ renderStart/renderers/verticalRenderer/v4/buildVerticalForm/6-createButtonElement.js
@@ -0,0 +1,24 @@
+const createButtonElement = ({ inCol, inButtonClass }) => {
+    const buttonOptions = inCol?.options?.verticalForm?.button || inCol?.options?.button || inCol?.options?.table?.tfoot?.inputsRow?.button || {};
+    const button = document.createElement("button");
+
+    const text = buttonOptions.text || buttonOptions.label || inCol?.options?.buttonText || "Submit";
+    button.textContent = text;
+    button.type = buttonOptions.type || "button";
+
+    const customClass = buttonOptions.className || inButtonClass;
+    if (customClass) button.className = customClass;
+    button.classList.add("ks-vertical-form-button");
+
+    if (buttonOptions.id) button.id = buttonOptions.id;
+    if (buttonOptions.name) button.name = buttonOptions.name;
+    if (buttonOptions.title) button.title = buttonOptions.title;
+
+    if (typeof buttonOptions.onClick === "function") {
+        button.addEventListener("click", (e) => buttonOptions.onClick(e, { column: inCol }));
+    }
+
+    return button;
+};
+
+export { createButtonElement };
